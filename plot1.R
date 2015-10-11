41045b2c217ce82fb63ece1153f5b5e6dfe7a990
sammnoh <sammnoh@gmail.com>
2015-10-11 15:58:06 -0400

    Upload plot1.R

---
+++ plot1.R
@@ -0,0 +1,15 @@
+URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(URL, destfile="./data4-1.zip", mode = "wb" )
+unzip("./data4-1.zip", exdir= "data4-1" )
+list.files("data4-1")
+
+full <- read.table("./data4-1/household_power_consumption.txt", header = TRUE, sep = ";")
+
+full$Date <- as.Date(full$Date, format = "%d/%m/%Y")
+full$Global_active_power <- as.numeric(as.character(full$Global_active_power))
+work <- subset(full, Date == "2007-02-02" | Date == "2007-02-01")
+
+png(file = "plot1.png")
+hist(work$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)", ylab="Frequency")
+dev.off()
+       
